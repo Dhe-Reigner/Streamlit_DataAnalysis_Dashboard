297496d2035a3df9b8a553cab9a7bfca287d2433
dhereigner <martinkagema01@gmail.com>
2026-01-20 09:49:56 +0300

    app.py

---
+++ src/app.py
@@ -0,0 +1,7 @@
+import streamlit as st
+import altair as alt
+import seaborn as sns
+import pandas as pd
+import plotly.express as px
+from matplotlib import pyplot as plt
+from streamlit_extras.dataframe_explorer import dataframe_explorer
